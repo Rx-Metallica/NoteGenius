99ce8244d778bf46b353a98d34aa7e0015bd7d32
Rx-Metallica <adikamble13337@gmail.com>
2025-10-20 00:43:55 +0530

    cahnges mafe in server.js

--- backend/server.js
+++ backend/server.js
@@ -23,5 +23,5 @@ app.use("/api/auth", authRoutes);
 app.use("/api/notes", notesRoutes);
 app.use("/api/ai",chatRoutes)
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
